38c82f3b75b99f3f8af6622b3ec92d79066227e3
agrawalkrishna9999-boop <agrawalkrishna9999@gmail.com>
2026-07-10 13:07:21 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { Hero } from "@/components/Hero"
+import { Services } from "@/components/Services"
+
+function App() {
+  return (
+    <div className="min-h-screen bg-background">
+      <Hero />
+      <Services />
+    </div>
+  )
+}
+
+export default App
